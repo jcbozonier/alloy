792095316be9961dd7694474eb844864753bf540
Justin Bozonier <darkxanthos@gmail.com>
2009-04-11 22:18:55 -0700

    Deleting stray files

--- IronTwit/IronTwit/Messaging/Services/IServiceResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿using Unite.Messaging.Entities;
-
-namespace Unite.Messaging.Services
-{
-    public interface IServiceResolver
-    {
-        ServiceInformation GetService(string address);
-    }
-}
