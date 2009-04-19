5b96480ff53f96951da12066fe95fb3dc02f16ab
Justin Bozonier <darkxanthos@gmail.com>
2009-04-19 01:21:23 -0700

    Simplified test a bit

--- IronTwit/Alloy/TwitterPlugIn.Specs/When_asking_if_a_plugin_can_accept_a_credential.cs
+++ IronTwit/Alloy/TwitterPlugIn.Specs/When_asking_if_a_plugin_can_accept_a_credential.cs
@@ -1,22 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using IronTwitterPlugIn;
 using NUnit.Framework;
 using Rhino.Mocks;
 using SpecUnit;
 using Unite.Messaging;
 using Unite.Messaging.Entities;
+using Unite.Specs.FakeSpecObjects;
 
 namespace TwitterPlugIn.Specs
 {
     [TestFixture]
     public class When_asking_if_a_plugin_can_accept_a_credential_for_another_service
     {
-        private TwitterUtilities Twitter;
-        private ITwitterDataAccess TwitterDal;
-        private bool CredentialsAccepted;
 
         [Test]
         public void The_plugin_should_reply_that_it_cant()
@@ -24,30 +19,29 @@ namespace TwitterPlugIn.Specs
             CredentialsAccepted.ShouldBeFalse();
         }
 
-        [TestFixtureSetUp]
-        public void Context()
-        {
-            TwitterDal = MockRepository.GenerateMock<ITwitterDataAccess>();
-            Twitter = new TwitterUtilities(TwitterDal);
-
-            BecauseOf();
-        }
-
         private void BecauseOf()
         {
             // Just creating some arbitrary credentials here that
             // obviously don't come from Twitter plug in and thus
             // can't possibly be acceptable.
-            CredentialsAccepted = Twitter.CanAccept(new Credentials()
-                                  {
-                                      UserName = "",
-                                      Password = "",
-                                      ServiceInformation = new ServiceInformation()
-                                                               {
-                                                                   ServiceID = Guid.NewGuid(),
-                                                                   ServiceName = "Name",
-                                                               }
-                                  });
+            CredentialsAccepted = Twitter.CanAccept(CredentialForDifferentService);
         }
+
+        [TestFixtureSetUp]
+        public void Context()
+        {
+            TwitterDal = MockRepository.GenerateMock<ITwitterDataAccess>();
+            Twitter = new TwitterUtilities(TwitterDal);
+            CredentialForDifferentService = new ScenarioRepository().CreateFakeCredentials();
+
+            BecauseOf();
+        }
+
+        
+
+        private TwitterUtilities Twitter;
+        private ITwitterDataAccess TwitterDal;
+        private bool CredentialsAccepted;
+        private Credentials CredentialForDifferentService;
     }
 }
