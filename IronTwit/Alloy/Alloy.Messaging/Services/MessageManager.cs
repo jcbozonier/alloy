001a70c1e41604d89654bdd8fd324592d7925456
Justin Bozonier <darkxanthos@gmail.com>
2009-05-18 21:43:55 -0700

    Cleaning up for the night.

--- IronTwit/Alloy/Alloy.Messaging/Services/MessageManager.cs
+++ IronTwit/Alloy/Alloy.Messaging/Services/MessageManager.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Windows.Threading;
 using Unite.Messaging.Entities;
 using Unite.Messaging.Extras;
 
---
+++ IronTwit/Alloy/Alloy.UI/Utilities/CredentialManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Unite.Messaging;
+using Unite.Messaging.Messages;
+
+namespace unite.ui.utilities
+{
+    public class CredentialManager
+    {
+        private IMessagingServiceManager _MessagingService;
+
+        public CredentialManager(IMessagingServiceManager messagingService)
+        {
+            _MessagingService = messagingService;
+            _MessagingService.CredentialsRequested += _MessagingService_CredentialsRequested;
+            _MessagingService.AuthorizationFailed += _MessagingService_AuthorizationFailed;
+        }
+
+        void _MessagingService_AuthorizationFailed(object sender, CredentialEventArgs e)
+        {
+            var del = AuthorizationFailed;
+            if (del != null)
+                del(this, e);
+        }
+
+        void _MessagingService_CredentialsRequested(object sender, CredentialEventArgs e)
+        {
+            var del = CredentialsRequested;
+            if (del != null)
+                del(this, e);
+        }
+
+        public event EventHandler<CredentialEventArgs> CredentialsRequested;
+        public event EventHandler<CredentialEventArgs> AuthorizationFailed;
+
+        public void SetCredentials(Credentials credentials)
+        {
+             _MessagingService.SetCredentials(credentials);
+        }
+    }
+}
